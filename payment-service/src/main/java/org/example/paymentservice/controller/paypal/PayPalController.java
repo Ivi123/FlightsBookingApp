eae844d9c797589150547e0ac2bc918ac4d8c33e
OanaBiancaTuca <tuca.oana.bianca30@gmail.com>
2024-04-10 09:31:20 +0300

    Added PayPal Configuration

--- payment-service/src/main/java/org/example/paymentservice/controller/paypal/PayPalController.java
+++ payment-service/src/main/java/org/example/paymentservice/controller/paypal/PayPalController.java
@@ -3,6 +3,7 @@ package org.example.paymentservice.controller.paypal;
 import org.example.paymentservice.model.paypal.CompletedOrder;
 import org.example.paymentservice.model.paypal.PaymentOrder;
 import org.example.paymentservice.service.paypal.PayPalService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
